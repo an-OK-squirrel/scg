a319fcc98360b0c8e7e58a77ad4c093c1c24bfd4
an-OK-squirrel <an-OK-squirrel@users.noreply.github.com>
2015-09-26 10:45:57 -0500

    things!

--- scg.py
+++ scg.py
@@ -2,3 +2,24 @@
 # yep, it's happening! :D
 # whee
 
+import sys
+from sparser import *
+
+a()
+'''
+try:
+	path = sys.argv[1]
+	f = open(path, 'r')
+	program = f.read()
+	f.close()
+
+	print(program)
+except Exception:
+	exit()
+
+'''
+
+# for now, programs are taken by text input due for debugging
+program = "Apples*gdAG*"
+
+print(split_into_st(program))
---
+++ sparser.py
@@ -0,0 +1,28 @@
+L_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+U_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DIGITS = "0123456789"
+ALL_LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+OPERATORS = "*"
+WHITESPACE = " \t"
+
+def find_rest_of_token():
+	
+
+def split_into_st(program): # split_into_st("apple") # ['a', 'p', 'p', 'l', 'e']
+	result = []
+	charIndex = 0
+
+	while charIndex < len(program):
+		current_token = ""
+		char = program[charIndex]
+		if char in DIGITS:
+			pass
+		elif char in DIGITS:
+			pass
+		elif char in WHITESPACE:
+			pass
+	return result
+
+
+def a():
+	print("hi")
