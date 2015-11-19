e98f0e492e3cc32b0e2dcccd2e25f3793d3eac36
an-OK-squirrel <an-OK-squirrel@users.noreply.github.com>
2015-11-19 15:47:52 -0600

    gh ready!

--- scg.py
+++ scg.py
@@ -6,26 +6,17 @@ import sys
 from inter import *
 from sparser import *
 
-'''
+
 try:
     path = sys.argv[1]
     f = open(path, 'r')
     program = f.read()
     f.close()
-    print(program)
 except Exception:
     exit()
 
-'''
 
-# for now, programs are taken by text input due for debugging
-# TODO: Add like the real thing
-program = '"a"2+'
-
-# print(fully_parse(program))
-# print(split_into_st(program))
 program = fully_parse(program)
 inter = Inter()
 inter.run_code(program)
 inter.output()
-# print(inter.stack)
