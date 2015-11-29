92a1e91577ee63dc2e0a2c6f3ebd9468c7ced8cb
an-OK-squirrel <an-OK-squirrel@users.noreply.github.com>
2015-11-29 14:39:17 -0600

    debugging, start of arrays

--- inter.py
+++ inter.py
@@ -6,10 +6,14 @@ class Token:
     def str_val(self):
         return str(self.value)
 
+    def __str__(self):
+        return str(self.value)
+
 
 class Inter:
     def __init__(self):
         self.stack = []
+        self.arr_markers = []  # array markers, thx aditsu
         self.variables = {}  # TODO: add more variables
 
     def run_code(self, code):
@@ -29,8 +33,8 @@ class Inter:
             if len(self.stack) < 2:
                 self.error('Not enough items on stack')
                 return
-            y = self.stack.pop()
-            x = self.stack.pop()
+            y = self.pop()
+            x = self.pop()
             if x.type == 'integer' and y.type == 'integer':
                 self.stack.append(Token('integer', x.value + y.value))
             elif x.type == 'string' or y.type == 'string':
@@ -39,8 +43,8 @@ class Inter:
             if len(self.stack) < 2:
                 self.error('Not enough items on stack')
                 return
-            y = self.stack.pop()
-            x = self.stack.pop()
+            y = self.pop()
+            x = self.pop()
             if x.type == 'integer' and y.type == 'integer':
                 self.stack.append(Token('integer', x.value - y.value))
             else:
@@ -50,8 +54,8 @@ class Inter:
             if len(self.stack) < 2:
                 self.error('Not enough items on stack')
                 return
-            y = self.stack.pop()
-            x = self.stack.pop()
+            y = self.pop()
+            x = self.pop()
             if x.type == 'integer' and y.type == 'integer':
                 self.stack.append(Token('integer', x.value * y.value))
             else:
@@ -61,8 +65,8 @@ class Inter:
             if len(self.stack) < 2:
                 self.error('Not enough items on stack')
                 return
-            y = self.stack.pop()
-            x = self.stack.pop()
+            y = self.pop()
+            x = self.pop()
             if x.type == 'integer' and y.type == 'integer':
                 self.stack.append(Token('integer', x.value / y.value))
             else:
@@ -72,8 +76,8 @@ class Inter:
             if len(self.stack) < 2:
                 self.error('Not enough items on stack')
                 return
-            y = self.stack.pop()
-            x = self.stack.pop()
+            y = self.pop()
+            x = self.pop()
             if x.value == y.value:
                 self.stack.append(Token('integer', 1))
             else:
@@ -82,8 +86,8 @@ class Inter:
             if len(self.stack) < 2:
                 self.error('Not enough items on stack')
                 return
-            exp = self.stack.pop()
-            base = self.stack.pop()
+            exp = self.pop()
+            base = self.pop()
             if exp.type == 'integer' and base.type == 'integer':
                 self.stack.append(Token('integer', base.value ** exp.value))
             else:
@@ -93,10 +97,22 @@ class Inter:
             if len(self.stack) < 1:
                 self.error('Not enough items on stack')
                 return
-            self.stack.pop()
+            self.pop()
+        if op == '[':
+            self.arr_markers.append(len(self.stack))
 
     def output(self):
         print("".join(list(map(lambda x: x.str_val(), self.stack))))
 
     def error(self, message):
         print('Error: ' + message)
+
+    def pop(self):
+        if len(self.arr_markers) > 0:
+            if self.arr_markers[-1] == len(self.stack):
+                self.arr_markers[-1] -= 1
+        return self.stack.pop()
+
+    def debug(self):
+        print('Stack: ' + str(self.stack))
+        print('Array Markers: ' + str(self.arr_markers))
